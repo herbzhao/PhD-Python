6c57a8f4241b2ae6d323096ee474212153c8d887
herbzhao <herbzhao@gmail.com>
2017-03-31 08:34:14 +0200

    plot humidity, temperature vs wavelength. export to csv sheet

--- spectrometer/read_scan.py
+++ spectrometer/read_scan.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 from scipy import signal
-
+import csv
+import re
 import peakutils
 
 
@@ -15,6 +16,10 @@ class read_spectrometer():
         self.background_substracted_scan_results = {}
         self.peak_wavelength = {}
         self.peak_intensity = {}
+
+        #regex
+        self.temperature_re = re.compile('T\d+.?\d*')
+        self.humidity_re = re.compile('H\d+.?\d*-')
         
         #settings
         self.wavelength_xlim = (300,800)
@@ -23,8 +28,10 @@ class read_spectrometer():
         # run functions
         self.convert_matlab_scan()
         self.format_result()
+        self.format_description()
         self.find_peaks()
         self.remove_background(background_sample_number = 0)
+        
 
 
     def convert_matlab_scan(self):
@@ -57,6 +64,27 @@ class read_spectrometer():
                 np.transpose(wavelength_data), 
                 np.transpose(spectrometer_data)))
             self.sample_description.append(description)
+   
+    def format_description(self):
+        print(self.sample_description)
+        self.sample_temperature = []
+        self.sample_humidity = []
+        for description in self.sample_description:
+            temperature = self.temperature_re.findall(description[0])
+            humidity = self.humidity_re.findall(description[0])
+            try:
+                #print(temperature[0])
+                temperature = temperature[0].replace('T','').replace('-','')
+            except IndexError: 
+                temperature = '' # if the data is empty, set it to 0
+            try:
+                #print(humidity[0])
+                humidity = humidity[0].replace('H','').replace('-','')
+            except IndexError:
+                humidity = '' # if the data is empty, set it to 0
+        
+            self.sample_temperature.append(temperature)
+            self.sample_humidity.append(humidity)
 
         #print(self.formatted_scan_results[3])
 
@@ -73,17 +101,19 @@ class read_spectrometer():
     def find_peaks(self):
         'find peak using scipy'
         for sample_number in range(self.total_sample_number):
-            peak_data_point = signal.find_peaks_cwt(
+            peak_data_points = signal.find_peaks_cwt(
                 self.formatted_scan_results[sample_number][1], 
                 np.arange(50,150))
-            peak_wavelengths = self.formatted_scan_results[sample_number][0][peak_data_point]
-            for peak_wavelength in peak_wavelengths:
+            for peak_data_point in peak_data_points:
+                peak_wavelength = float(self.formatted_scan_results[sample_number][0][peak_data_point])
                 if peak_wavelength > 300 and peak_wavelength < 800:
                     self.peak_wavelength[sample_number] = peak_wavelength
+                    #print(self.peak_wavelength[sample_number])
+                    # self.peak_intensity only needs one data
                     self.peak_intensity[sample_number] = self.formatted_scan_results[sample_number][1][peak_data_point]
+                    #print(self.peak_intensity[sample_number])
 
-
-    def find_peaks_2(self):
+    '''def find_peaks_2(self):
         'find peak using peakutils'
         for sample_number in range(self.total_sample_number):
             peak_data_point = peakutils.indexes(
@@ -92,7 +122,7 @@ class read_spectrometer():
             peak_wavelengths = self.formatted_scan_results[sample_number][0][peak_data_point]
             for peak_wavelength in peak_wavelengths:
                 if peak_wavelength > 300 and peak_wavelength < 800:
-                    self.peak_2[sample_number] = peak_wavelength
+                    self.peak_2[sample_number] = peak_wavelength'''
 
     def plot_spectrum(self, sample_number):
         'plot spectra with given sample number, show peaks'
@@ -110,10 +140,7 @@ class read_spectrometer():
                     sample_number + 1, self.sample_description[sample_number]))        
         
         if self.show_peaks is True:
-            ax.plot((self.peak_wavelength[sample_number],self.peak_wavelength[sample_number]),(0,1.1), label = 'scipy peaks')
-            print(self.peak_wavelength[sample_number])
-            print(self.peak_intensity[sample_number])
-
+            ax.plot((self.peak_wavelength[sample_number],self.peak_wavelength[sample_number]), (0,1.1), label = 'scipy peaks')
 
         # set range etc
         ax.set_xlim(self.wavelength_xlim)
@@ -125,10 +152,34 @@ class read_spectrometer():
             fontsize=12, loc=1, bbox_to_anchor=(1.1,1))
 
     def plot_peaks(self):
-        ax2 = plt.subplot(221)
+        'plot spectra with given sample number, show peaks'
+        ax2 = plt.subplot(212)
+        ax2.set_ylim(0,100)
+        #ax2.set_xlim(self.spec_ylim)
         for sample_number in range(self.total_sample_number):
-            ax2.scatter(self.peak_wavelength[sample_number], self.peak_intensity[sample_number])
+            #print(self.sample_humidity[sample_number])
+            #ax2.scatter(self.peak_wavelength[sample_number], self.peak_intensity[sample_number])
+            #ax2.scatter(float(self.sample_humidity[sample_number]),self.peak_wavelength[sample_number])
+            if self.sample_humidity[sample_number] == '':
+                pass
+            else:
+                ax2.scatter(self.peak_wavelength[sample_number], float(self.sample_humidity[sample_number]))
         
+
+    def export_data(self, filepath):
+        with open(filepath, 'w', newline="") as csvfile:
+            csv_writer = csv.writer(csvfile, dialect = 'excel')
+            csv_writer.writerow(['sample number','sample description','temperature','humidity','wavelength','intensity'])
+            for sample_number in range(self.total_sample_number):
+                csv_writer.writerow(
+                    ['{}'.format(sample_number + 1) ,
+                    '{}'.format(self.sample_description[sample_number]),
+                    '{}'.format(self.sample_temperature[sample_number]),
+                    '{}'.format(self.sample_humidity[sample_number]),
+                    '{}'.format(self.peak_wavelength[sample_number]),
+                    '{}'.format(self.peak_intensity[sample_number])])
+
+
 
 if __name__ == "__main__":
 # visualisation
@@ -136,5 +187,7 @@ if __name__ == "__main__":
     spectrometer_result.background_substraction = True
     spectrometer_result.show_peaks = True
     spectrometer_result.plot_spectrum(30)
-    #spectrometer_result.plot_peaks()
+    spectrometer_result.plot_peaks()
+    spectrometer_result.export_data(r'C:\Users\herbz\OneDrive - University Of Cambridge\Documents\GitHub\PhD-python\spectrometer\scan.csv')
+    spectrometer_result.export_data(r'scan_2.csv')
     plt.show()
