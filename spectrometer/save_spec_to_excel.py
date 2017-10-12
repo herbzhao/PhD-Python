9a9ed3de4664c99a3f49eb38299a51374ec6ca17
herbzhao <herbzhao@gmail.com>
2017-10-12 09:14:11 +0100

    save to csv by splitting horizontally

--- spectrometer/save_spec_to_excel.py
+++ spectrometer/save_spec_to_excel.py
@@ -46,15 +46,47 @@ class read_spectrometer():
         # rearrange the header
         header = self.df.columns.tolist()
         header = header[-1:] + header[:-1]
-        self.df = self.df[header]00000000000
+        self.df = self.df[header]
         
         #writer = pd.ExcelWriter(self.folder +'\\'+ 'output_smoothed_peaks.xlsx')
         self.df.to_csv(self.folder +'\\'+ self.output_filename,  sep=',')
 
+
+
+    def convert_matlab_scan_2(self):
+        'this converts matlab data into np.ndarray '
+        self.mat_scan_contents = sio.loadmat(self.folder +'\\'+ self.scan_filename)['scan']
+        self.scan_result = {}
+        # catagorise scan result into spectrum, description and wavelength
+        for i in ['spec', 'desc', 'wl']:
+            self.scan_result[i] = self.mat_scan_contents[i]
+        # total scan numbers
+        self.total_scan_number = len(self.scan_result['wl'])
         
+        table = {}
+        # all the spectra share the same wavelength axis
+        table['wavelength'] = []
+        # convert numpy array into a list 
+        list(map(lambda wavelength: table['wavelength'].append(float(wavelength)), self.scan_result['wl'][0][0]))
+
+        # assign spectra intensity to sample description into columns
+        for sample_number in range(len(self.scan_result['desc'])):
+            table[self.scan_result['desc'][sample_number][0][0]] = []
+            list(map(lambda intensity: table[self.scan_result['desc'][sample_number][0][0]].append(float(intensity)), self.scan_result['spec'][sample_number][0]))
+        
+        self.df = pd.DataFrame(table)
+        
+        # reverse the header order
+        header = self.df.columns.tolist()[::-1]
+        print(self.df[header])
+        #writer = pd.ExcelWriter(self.folder +'\\'+ 'output_smoothed_peaks.xlsx')
+        self.df.to_csv(self.folder +'\\'+ self.output_filename,  sep=',')
     
+
+
+
 scan = read_spectrometer()
 scan.folder = r'D:\GDrive\Research\BIP\Humidity sensor project\data\20170810 - temper'
 scan.scan_filename = 'smoothed_peaks.mat'
 scan.output_filename = 'output_smoothed_peaks.csv'
-scan.convert_matlab_scan()
+scan.convert_matlab_scan_2()
